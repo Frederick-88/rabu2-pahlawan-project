10bf1ba798e364dc3f9badc0451df46645df9434
Frederick-88 <frederickfd88@gmail.com>
2020-04-15 19:07:27 +0700

    COMMIT 3

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useFormik } from "formik";
-import axios from 'axios';
+import axios from "axios";
+import { Card } from "react-bootstrap";
+import "./index.css";
 
 const FormPahlawan = () => {
   const formik = useFormik({
@@ -14,71 +16,100 @@ const FormPahlawan = () => {
     },
     onSubmit: (values) => {
       console.log(values);
-      axios.post('http://localhost:8000/dataJSON',{values}).then(res => {
-        console.log(res);
-        
-      })
+      axios.post("http://localhost:8000/dataJSON", { values }).then((res) => {
+        window.alert("BERHASIL MENAMBAH DATA.");
+      });
     },
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Full Name : </label>
-      <input
-        name="fullname"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.fullname}
-      ></input>
+    <div className="div-bg">
+      <Card className="card-bg">
+        <Card.Img
+          className="img-card"
+          variant="top"
+          src="https://image.shutterstock.com/image-vector/illustration-abstract-texture-blocks-indonesia-600w-1439717390.jpg"
+        />
+        <Card.Body>
+          <form onSubmit={formik.handleSubmit}>
+            <label></label>
+            <input
+              className="input-fx"
+              name="fullname"
+              placeholder="Full Name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.fullname}
+            ></input>
 
-      <br />
-      <label>Born : </label>
-      <input
-        name="born"
-        type="number"
-        onChange={formik.handleChange}
-        value={formik.values.born}
-      ></input>
+            <br />
+            <label></label>
+            <input
+              className="input-fx"
+              name="born"
+              placeholder="Born"
+              type="number"
+              onChange={formik.handleChange}
+              value={formik.values.born}
+            ></input>
 
-      <br />
-      <label>Died : </label>
-      <input
-        name="died"
-        type="number"
-        onChange={formik.handleChange}
-        value={formik.values.died}
-      ></input>
+            <br />
+            <label></label>
+            <input
+              className="input-fx"
+              name="died"
+              placeholder="Died"
+              type="number"
+              onChange={formik.handleChange}
+              value={formik.values.died}
+            ></input>
 
-      <br />
-      <label>Description : </label>
-      <input
-        name="description"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.description}
-      ></input>
+            <br />
+            <label></label>
+            <input
+              className="input-fx"
+              name="description"
+              placeholder="Description"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.description}
+            ></input>
 
-      <br />
-      <label>Establishment : </label>
-      <input
-        name="establishment"
-        type="number"
-        onChange={formik.handleChange}
-        value={formik.values.establishment}
-      ></input>
+            <br />
+            <label></label>
+            <input
+              className="input-fx"
+              name="establishment"
+              placeholder="Establishment"
+              type="number"
+              onChange={formik.handleChange}
+              value={formik.values.establishment}
+            ></input>
 
-      <br />
-      <label>ImageURL : </label>
-      <input
-        name="imageURL"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.imageURL}
-      ></input>
+            <br />
+            <label></label>
+            <input
+              className="input-fx"
+              name="imageURL"
+              placeholder="ImageURL"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.imageURL}
+            ></input>
 
-      <br />
-      <button type="submit">SUBMIT</button>
-    </form>
+            <br />
+            <button className="button-fx" type="submit">
+              Submit
+            </button>
+            
+            <Link to="/dashboard">
+              <button type="button">Click Me!</button>
+            </Link>
+
+          </form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
